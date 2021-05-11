33f274426baed51b5caa34de4357fc00de926b5f
Gabriel Nunes Henke <gabrielchicao3@gmail.com>
2021-05-11 15:30:24 -0300

    Atualizações de sql e model

---
+++ api/src/sql/querino2.sql
@@ -0,0 +1,180 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: querino
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bairro`
+--
+
+DROP TABLE IF EXISTS `bairro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bairro` (
+  `id` int NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bairro`
+--
+
+LOCK TABLES `bairro` WRITE;
+/*!40000 ALTER TABLE `bairro` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bairro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cidade`
+--
+
+DROP TABLE IF EXISTS `cidade`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cidade` (
+  `id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cidade`
+--
+
+LOCK TABLES `cidade` WRITE;
+/*!40000 ALTER TABLE `cidade` DISABLE KEYS */;
+INSERT INTO `cidade` VALUES (1,'Pompeia');
+/*!40000 ALTER TABLE `cidade` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `combustivel`
+--
+
+DROP TABLE IF EXISTS `combustivel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `combustivel` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `preco` decimal(10,2) NOT NULL,
+  `posto_id` int NOT NULL,
+  `combustivel_type_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_combustivel_type1_idx` (`combustivel_type_id`),
+  KEY `fk_combustivel_posto1_idx` (`posto_id`),
+  CONSTRAINT `fk_combustivel_posto1` FOREIGN KEY (`posto_id`) REFERENCES `posto` (`id`),
+  CONSTRAINT `fk_combustivel_type1` FOREIGN KEY (`combustivel_type_id`) REFERENCES `combustivel_type` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `combustivel`
+--
+
+LOCK TABLES `combustivel` WRITE;
+/*!40000 ALTER TABLE `combustivel` DISABLE KEYS */;
+INSERT INTO `combustivel` VALUES (1,4.00,1,1),(3,4.46,1,2),(4,4.50,1,3);
+/*!40000 ALTER TABLE `combustivel` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `combustivel_type`
+--
+
+DROP TABLE IF EXISTS `combustivel_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `combustivel_type` (
+  `id` int NOT NULL,
+  `name` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `combustivel_type`
+--
+
+LOCK TABLES `combustivel_type` WRITE;
+/*!40000 ALTER TABLE `combustivel_type` DISABLE KEYS */;
+INSERT INTO `combustivel_type` VALUES (1,'Gasolina'),(2,'Etanol'),(3,'Diesel');
+/*!40000 ALTER TABLE `combustivel_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `endereco`
+--
+
+DROP TABLE IF EXISTS `endereco`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `endereco` (
+  `id` int NOT NULL,
+  `name` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `endereco`
+--
+
+LOCK TABLES `endereco` WRITE;
+/*!40000 ALTER TABLE `endereco` DISABLE KEYS */;
+/*!40000 ALTER TABLE `endereco` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posto`
+--
+
+DROP TABLE IF EXISTS `posto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posto` (
+  `id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `latitude` int DEFAULT NULL,
+  `longitude` int DEFAULT NULL,
+  `cidade_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_posto_cidade_idx` (`cidade_id`),
+  CONSTRAINT `fk_posto_cidade` FOREIGN KEY (`cidade_id`) REFERENCES `cidade` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posto`
+--
+
+LOCK TABLES `posto` WRITE;
+/*!40000 ALTER TABLE `posto` DISABLE KEYS */;
+INSERT INTO `posto` VALUES (1,'Aurora',123,321,1);
+/*!40000 ALTER TABLE `posto` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-11 15:30:00
