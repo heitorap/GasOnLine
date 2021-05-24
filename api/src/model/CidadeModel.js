dbf663de20969d3a1671f7e1cd5e05fb47603883
Pedro <pedrotzf@gmail.com>
2021-05-24 16:19:17 -0300

    feat(CidadeModel.js): primeira query que tras todos os postos
    seus combustiveis e dados da cidade

---
+++ api/src/model/CidadeModel.js
@@ -0,0 +1,45 @@
+import db from '../config/database';
+
+const cidadeModel = {
+    listar() {
+        const query = `
+         SELECT CI.id   AS "id_cidade",
+                PO.id   AS "id_posto",
+                PO.name AS "posto",
+                 (
+                    SELECT CO.preco
+                      FROM combustivel AS CO
+                INNER JOIN combustivel_type AS CT
+                        ON CT.id = CO.combustivel_type_id
+                     WHERE CO.posto_id = PO.id
+                       AND CT.id = 1) AS "gasolina",
+                 (
+                    SELECT CO.preco
+                      FROM combustivel AS CO
+                INNER JOIN combustivel_type AS CT
+                        ON CT.id = CO.combustivel_type_id
+                     WHERE CO.posto_id = PO.id
+                       AND CT.id = 2) AS "etanol",
+                 (
+                    SELECT CO.preco
+                      FROM combustivel AS CO
+                INNER JOIN combustivel_type AS CT
+                        ON CT.id = CO.combustivel_type_id
+                     WHERE CO.posto_id = PO.id
+                       AND CT.id = 3) AS "diesel"
+           FROM cidade AS CI
+     INNER JOIN posto AS PO
+             ON PO.cidade_id = CI.id;`;
+        return new Promise((resolve, reject) => {
+            db.query(query, (error, result) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    }
+};
+
+export default cidadeModel;
