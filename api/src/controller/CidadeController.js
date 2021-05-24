5c53f6b329d9ed8f337e998facd85e084d71f8a0
Pedro <pedrotzf@gmail.com>
2021-05-24 17:33:31 -0300

    feat(CidadeController.js): controller que lista as cidades

--- api/src/controller/CidadeController.js
+++ api/src/controller/CidadeController.js
@@ -1,8 +1,18 @@
 import cidadeModel from '../model/CidadeModel';
 
 const cidadeController = {
+    listar(req, res) {
+        cidadeModel.listar()
+            .then((response) => {
+                res.status(200).send(response);
+            })
+            .catch((error) => {
+                res.status(500).send(error);
+            })
+    },
+    
     listarPostos(req, res) {
-        cidadeModel.listar(req.body.cidade)
+        cidadeModel.listarPostos(req.body.cidade)
             .then((response) => {
                 res.status(200).send(response);
             })
