d1cde71b964245e4ff2e1734caeefd48624054c1
Pedro <pedrotzf@gmail.com>
2021-05-24 17:28:14 -0300

    refactor(CidadeModel.js): corrigindo detalhes

--- api/src/model/CidadeModel.js
+++ api/src/model/CidadeModel.js
@@ -1,27 +1,28 @@
 import db from '../config/database';
 
 const cidadeModel = {
-    listar(nomeCidade = '%') {
-      const cidade = nomeCidade ? nomeCidade + '%' : '';
+    listar(nomeCidade = '') {
+      const cidade = nomeCidade ? nomeCidade + '%' : '%';
       const query = `
        SELECT CI.id   AS "id_cidade",
+              CI.name AS "cidade",
               PO.id   AS "id_posto",
               PO.name AS "posto",
-               (
+              (
                   SELECT CO.preco
                     FROM combustivel AS CO
               INNER JOIN combustivel_type AS CT
                       ON CT.id = CO.combustivel_type_id
                    WHERE CO.posto_id = PO.id
                      AND CT.id = 1) AS "gasolina",
-               (
+              (
                   SELECT CO.preco
                     FROM combustivel AS CO
               INNER JOIN combustivel_type AS CT
                       ON CT.id = CO.combustivel_type_id
                    WHERE CO.posto_id = PO.id
                      AND CT.id = 2) AS "etanol",
-               (
+              (
                   SELECT CO.preco
                     FROM combustivel AS CO
               INNER JOIN combustivel_type AS CT
