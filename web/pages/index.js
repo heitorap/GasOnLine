549407decbc093d350f2ceb875a6bae39533c17a
Mateus Teodoro <mateus@MacBook-Air-de-Mateus.local>
2021-06-04 10:08:48 -0300

    Atualizando a pagina

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,11 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 // import { ErrorToast } from "../components/Toast/Toast";
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+import posto from '../api/posto';
 
 export default function Home() {
+  const [postos, setPostos] = useState([]);
+
+  const getPostos = () => {
+    posto.getPosto().then(response => {
+        setPostos(response.data);
+    }).catch(error => {
+        alert('Ocorreu um erro ao buscar os postos.');
+    });
+}
+
+
+  useEffect(() => {
+    getPostos();
+  }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
